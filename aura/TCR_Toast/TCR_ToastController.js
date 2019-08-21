84444f70ac665f6967f8acfeac6d312569220078
Szakalakazo <tomek.starzak@gmail.com>
2019-08-21 11:50:03 +0200

    create and refactor toas component

--- aura/TCR_Toast/TCR_ToastController.js
+++ aura/TCR_Toast/TCR_ToastController.js
@@ -6,19 +6,5 @@
             "message": params.message,
             "mode": "dismissable"
         });
-/*
-        let messageType = "";
-        let messageTitle = "";
-        let message = "";
-        component.set("v.message", params.message);
-        component.set("v.messageType", params.messageType);*/
-
-       /* let toastEvent = $A.get("e.force:showToast");
-        toastEvent.setParams({
-            "type": messageType,
-            "title": messageTitle,
-            "message": message
-        });
-        toastEvent.fire();*/
     }
 })
